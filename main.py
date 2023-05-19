98f18b6448d1443b257cd4dd4288aad8cb8898f0
Cassiano de Ramos <cassianoramos48@gmail.com>
2023-05-19 20:54:23 -0300

    Versão 1.6
    Foi adicionado o texto de opções do usuário

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from tkinter import simpledialog
 #Configurações iniciais
 pygame.init()
 tamanho = (883,600)
+branco = (255,255,255)
 tela = pygame.display.set_mode(tamanho)
 pygame.display.set_caption("Space Marker")
 running = True
@@ -12,6 +13,7 @@ pygame.display.set_icon( icone )
 fundo  = pygame.image.load("BG.jpeg")
 pygame.mixer.music.load("Interstellar.mp3")
 pygame.mixer.music.play(-1)
+font = pygame.font.Font(None, 20)
 
 while running:
     for event in pygame.event.get():
@@ -26,6 +28,14 @@ while running:
             if item == None:
                 item = "desconhecido"+str(pos)
             estrelas[item] = pos
+
     tela.blit( fundo, (0,0) )
+    salvar = font.render("Pressione F10 para Salvar os pontos", True, branco)
+    carregar = font.render("Pressione F11 para Carregar os pontos", True, branco)
+    deletar = font.render("Pressione F12 para Deletar os pontos", True, branco)
+    tela.blit( salvar, (10, 10) )
+    tela.blit( carregar, (10, 22) )
+    tela.blit( deletar, (10, 32) )
+    
     pygame.display.update()
 pygame.quit()
